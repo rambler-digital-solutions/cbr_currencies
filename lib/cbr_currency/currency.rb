ed1858dc3c8a0f28162bd762e63d8239a8f9dd6a
Petrova Y <janavpetrova@gmail.com>
2015-07-22 18:32:23 +0300

    normalize value: how much rubles you should give for the foreign coin

--- lib/cbr_currency/currency.rb
+++ lib/cbr_currency/currency.rb
@@ -4,19 +4,14 @@ module CbrCurrency
 
     def initialize(char_code:, value:, title:, nominal:)
       @char_code = char_code.downcase.to_sym
-      @value = normalize_value value
+      @value = normalize_value value, nominal
       @title = title
-      @nominal = normalize_nominal nominal
     end
 
     private
 
-    def normalize_value(value)
-      value.gsub(',', '.').to_f
-    end
-
-    def normalize_nominal(nominal)
-      nominal.to_i
+    def normalize_value(value, nominal)
+      value.gsub(',', '.').to_f / nominal.to_f
     end
   end
 end
--- spec/cbr_currency/currency_spec.rb
+++ spec/cbr_currency/currency_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe CbrCurrency::Currency do
-  let(:params) { { char_code: 'EUR', value: '65,50', title: 'Евро', nominal: '1' } }
+  let(:params) { { char_code: 'EUR', value: '62,60', title: 'Евро', nominal: '2' } }
   let(:currency) { CbrCurrency::Currency.new(params) }
 
   describe '.new' do
@@ -9,6 +9,6 @@ RSpec.describe CbrCurrency::Currency do
 
     it { expect(subject.char_code).to be_a_kind_of Symbol }
     it { expect(subject.value).to be_a_kind_of Float }
-    it { expect(subject.nominal).to be_a_kind_of Integer }
+    it { expect(subject.value).to eq 31.3 }
   end
 end
